9d1425611ba174a4fd04b670df906cfecf0ba160
Calum <cjmcguicken@gmail.com>
2018-09-07 14:15:07 +0200

    Added comments to main

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ filePath = "data/beerReviews.csv"
 #Percentage of dataset for training
 testPercentage = 80
 
+
+#Create the beer model
 beerModel = Model(filePath)
-
+#Split beer review data into Test and Train based on user input
 beerModel.splitData(testPercentage)
-
+#Display bar chart of Train score distribution
 beerModel.displayScore()
 
 print(beerModel.cleanText("This a review of a beer calld Rosebeer. I thank it is rubbsh but it had a gud aroma!!! It's bettr than a fish that is fishin in a fishery."))
